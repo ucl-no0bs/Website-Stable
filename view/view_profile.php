53de82fe4409bbf1a278acb935d28ee5129ab35a
Jay Jeyaruban <jay.jeyaruban.15@ucl.ac.uk>
2017-01-11 20:54:02 +0000

    UI changes.

--- view/view_profile.php
+++ view/view_profile.php
@@ -1,69 +1,47 @@
-<!DOCTYPE html>
-<html>
+<?php session_start(); ?>
 
-<head>
-  <title>Home Page</title>
-  <?php include_once("import_materialize.php"); ?>
-</head>
+  <!DOCTYPE html>
+  <html>
 
-<body>
+  <head>
+    <title>Home Page</title>
+    <?php include_once("import_materialize.php"); ?>
+  </head>
 
-  <?php include_once("import_navbar.php"); ?>
+  <body>
 
-  <div class="container section">
-    <ul class="collection with-header">
-      <li class="collection-header">USERNAME
-        <div class="secondary-content">
-          <i class="center"><label>PEANUTS</label></i>
-          <a class="modal-trigger" href="#modal">
-            <i class="material-icons right">mode_edit</i></a>
-        </div>
-      </li>
-      <li class="collection-item">FIRST NAME
-        <div class="secondary-content">
-          <i><label>JOHN</label></i>
-          <a class="modal-trigger" href="#modal">
-            <i class="material-icons right">mode_edit</i></a>
-        </div>
-      </li>
-      <li class="collection-item">LAST NAME
-        <div class="secondary-content">
-          <i><label>DOE</label></i>
-          <a class="modal-trigger" href="#modal">
-            <i class="material-icons right">mode_edit</i></a>
-        </div>
-      </li>
-      <li class="collection-item">PASSWORD
-        <div class="secondary-content">
-          <i><label>*******</label></i>
-          <a class="modal-trigger" href="#modal">
-            <i class="material-icons right">mode_edit</i></a>
-        </div>
-      </li>
-    </ul>
-  </div>
+    <?php include_once("import_navbar.php"); ?>
 
-  <div id="modal" class="modal modal-fixed-footer">
-    <div content="modal-content">
-      <h4>DATA FIELD</h4>
-      <h5>CHANGES</h5>
-    </div>
-    <div class="modal-footer">
-      <div class="right">
-        <button class="btn modal-action modal-close waves-effect waves-light">Confirm</button>
-        <button class="btn modal-action modal-close waves-effect waves-light">Cancel</button>
+      <div class="container section">
+        <ul class="collection with-header">
+          <li class="collection-item">PASSWORD
+            <div class="secondary-content">
+              <i><label>*******</label></i>
+              <a class="modal-trigger" href="#modal">
+                <i class="material-icons right">mode_edit</i></a>
+            </div>
+          </li>
+        </ul>
       </div>
-    </div>
-  </div>
 
-  <!--Import jQuery before materialize.js-->
-  <?php include_once("import_js.php"); ?>
-  <script language="Javascript">
-    $(document).ready(function() {
-      // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-      $('.modal').modal();
-    });
-  </script>
-</body>
+      <div id="modal" class="container modal bottom-sheet">
+        <div class="modal-content">
+          <label><b><i>Change Password:</i></b></label>
+          <input type="text" name="snippet" placeholder="Enter new password" />
+          <button class="btn modal-action modal-close waves-effect waves-light">Confirm</button>
+          <button class="btn modal-action modal-close waves-effect waves-light">Cancel</button>
+        </div>
+      </div>
+      </div>
 
-</html>
+      <!--Import jQuery before materialize.js-->
+      <?php include_once("import_js.php"); ?>
+        <script language="Javascript">
+          $(document).ready(function() {
+            // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+            $('.modal').modal();
+          });
+        </script>
+  </body>
+
+  </html>
