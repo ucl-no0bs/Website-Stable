50a1f6ef1d30ad3c94ac7036f3168744f041dd67
robobot97 <arjunkhurana26@hotmail.com>
2017-01-11 03:54:45 +0000

    Added code for retriving data from database

--- model/model_home.php
+++ model/model_home.php
@@ -7,6 +7,13 @@ include_once('model_safety.php');
 returns all snippets from a user
 */
 
+/*
+$result = mysql_query('SELECT * WHERE username=session_username');
+if (!$result) {
+    die('Invalid query: ' . mysql_error());
+}
+*/
+
 /* function
 adds a snippet from a certain user
 */
--- model/model_login.php
+++ model/model_login.php
@@ -11,7 +11,12 @@ function check_user($username, $password){
 
 // for ref, what a sql query looked like in my previous project:
 // $sql = "INSERT INTO objects (name,id,OS) VALUES ('$name_','$id_','$os_')";
-
+/*
+$result = mysql_query('SELECT username, password FROM database WHERE username=input_username && password=input_password');
+if (!$result) {
+    die('Incorrect Login details, try again.');
+}
+*/
 
 echo "model_login successfully included !"."<br />";
 
