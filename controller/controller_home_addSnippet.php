93c5115d7c008e85086d2e4c5d4af9a262ef9fb2
Jay Jeyaruban <jay.jeyaruban.15@ucl.ac.uk>
2017-01-12 21:52:36 +0000

    Sanitized snippet adding.

--- controller/controller_home_addSnippet.php
+++ controller/controller_home_addSnippet.php
@@ -5,7 +5,9 @@ include_once('../model/db_connection.php');
 
 include_once('../model/model_home.php');
 
-add_snippet($_SESSION['current_user'], $_POST['snippet'], $_POST['isPublic']);
+$sanitized_snippet = sanitize($_POST['snippet']);
+
+add_snippet($_SESSION['current_user'], $sanitized_snippet, $_POST['isPublic']);
 
 //echo "<script> window.location.assign('../view/view_home.php'); </script>";
 echo "<a href='../view/view_home.php'>Go to home page</a>";
