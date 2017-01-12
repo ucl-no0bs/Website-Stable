50e5a7c237c1b4b32cc80058a9bfa4dfddb7ed33
Jay Jeyaruban <jay.jeyaruban.15@ucl.ac.uk>
2017-01-12 12:46:18 +0000

    Comment edit

--- model/model_login.php
+++ model/model_login.php
@@ -4,13 +4,15 @@
 include_once('model_safety.php');
 
 
-// checks if the user/password duo is in the database
+// checks if the user duo is in the database
 function check_user($username, $password){
     global $conn;
     echo "the values passed are ".$username." and ".$password." <br />";
     //$sql_query = "SELECT count(username) FROM users WHERE username='$username' AND password='$password'";
     $sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
     $result = mysqli_query($conn, $sql);
+
+    echo $result;
 
     if(mysqli_num_rows($result) > 0){
         echo "some result !";
