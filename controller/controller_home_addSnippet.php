14a7df4fd80c53826fb9b166042940665e8ec723
Aksel <akselcakmak@gmail.com>
2017-01-13 15:17:15 +0000

    more htaccess stuff

--- controller/controller_home_addSnippet.php
+++ controller/controller_home_addSnippet.php
@@ -9,7 +9,7 @@ $sanitized_snippet = sanitize($_POST['snippet']);
 
 add_snippet($_SESSION['current_user'], $sanitized_snippet, $_POST['isPublic']);
 
-//echo "<script> window.location.assign('../view/view_home.php'); </script>";
-echo "<a href='../view/view_home.php'>Go to home page</a>";
+echo "<script> window.location.assign('../view/view_home.php'); </script>";
+//echo "<a href='../view/view_home.php'>Go to home page</a>";
 
  ?>
