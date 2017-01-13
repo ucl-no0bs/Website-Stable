627ed9df3e8b6c419f1b5bb66695bd61c2e341da
Jay Jeyaruban <jay.jeyaruban.15@ucl.ac.uk>
2017-01-13 14:14:27 +0000

    Admin redirect.

--- controller/controller_login.php
+++ controller/controller_login.php
@@ -10,20 +10,27 @@ include_once('../model/model_login.php');
 // Satinitizes the input (in the safe version)
 
 // Checks the database if username/pw exist by using the functions in model/model_login.php
-$user_exists = check_user($_GET['username'], $_GET['password']);
 
-if($user_exists) {
-    echo "user exists"."<br />";
-    $_SESSION["current_user"] = $_GET['username'];
-    //echo "<script> window.location.assign('../view/view_home.php'); </script>";
-    echo "<a href='../view/view_home.php'>Go to home page</a>";
+if ($_GET['username'] === "admin" && $_GET['password'] === "admin") {
+    echo "Hello admin..<br>"
+    echo "Redirecting to admin page..<br>"
+    echo "<script> window.location.assign('../view/view_admin.php'); </script>";
 } else {
-    echo "user/password doesn't exist";
-    echo "<script> window.location.assign('abc'); </script>";
+    $user_exists = check_user($_GET['username'], $_GET['password']);
+    
+    if($user_exists) {
+        echo "user exists"."<br />";
+        $_SESSION["current_user"] = $_GET['username'];
+        //echo "<script> window.location.assign('../view/view_home.php'); </script>";
+        echo "<a href='../view/view_home.php'>Go to home page</a>";
+    } else {
+        echo "user/password doesn't exist";
+        echo "<script> window.location.assign('abc'); </script>";
+    }
+    
+    // if it s correct: set the global $_SESSION username, and then redirect to view_home.php
+    // else, redirect to index.php
+    // note: don't redirect to login.php directly !
+    
 }
-
-// if it s correct: set the global $_SESSION username, and then redirect to view_home.php
-// else, redirect to index.php
-// note: don't redirect to login.php directly !
-
- ?>
+?>
