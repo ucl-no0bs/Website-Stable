8486ff89ec3c59c8b5611ba58100bc9458557a42
Jay Jeyaruban <jay.jeyaruban.15@ucl.ac.uk>
2017-01-13 14:16:37 +0000

    Blank admin page added.

---
+++ view/view_admin.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Admin Portal</title>
+  <?php include_once('import_materialize.php'); ?>
+</head>
+<body>
+
+
+
+<?php include_once("import_js.php"); ?>
+
+</body>
+</html>
