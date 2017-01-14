9c26df8cd9cc1784a2c24ed96cbee6edc0862a2f
Aksel <akselcakmak@gmail.com>
2017-01-14 16:53:56 +0000

    show/add should now be fixed

--- controller/controller_home_showSnippets.php
+++ controller/controller_home_showSnippets.php
@@ -5,7 +5,7 @@ include_once('../model/db_connection.php');
 include_once('../model/model_home.php');
 
 // Gets all your private snippets
-$private_snippets = get_snippets($_SESSION["current_user"], 'False');
+$private_snippets = get_snippets($_SESSION["current_user"], '0');
 $count = 1;
 while($row = mysqli_fetch_assoc($private_snippets)){
     echo '<li>
@@ -15,7 +15,7 @@ while($row = mysqli_fetch_assoc($private_snippets)){
 }
 
 echo "<h4>Public Notes:</h4>";
-$public_snippets = get_snippets($_SESSION["current_user"], 'True');
+$public_snippets = get_snippets($_SESSION["current_user"], '1');
 while($row2 = mysqli_fetch_assoc($public_snippets)){
     echo '<li>
             <div class="collapsible-header active">Note from: ' . $row2['username'] . '</div>
